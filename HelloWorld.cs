0ee1cfa9e934bb1e688f3f04ee10c64a9520c2c4
NikitaTairov <150437695+NikitaTairov@users.noreply.github.com>
2023-11-10 14:59:46 +0400

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Hello, World!"); //Выводит Hello, World! в консоль
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
---
+++ ObjectDestroy.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectDestroy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject); //Уничтожает объект
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
